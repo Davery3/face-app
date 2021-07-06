1c6e5ef834753aa6293e04f50c40a1ff58f4ccd5
Davery3 <80860555+Davery3@users.noreply.github.com>
2021-07-06 16:22:41 -0700

    edit

--- src/index.js
+++ src/index.js
@@ -11,7 +11,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
+// npm run host: run in local host
+// npm run kill: kills port 3000
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals

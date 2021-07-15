0cb8af8f3cb3f270422209ad3d85d521fc0218d1
Davery3 <80860555+Davery3@users.noreply.github.com>
2021-07-14 17:09:48 -0700

    edit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const particleSettings = {
         "value": 194,
         "density": {
           "enable": true,
-          "value_area": 800
+          "value_area": 1400
         }
       },
       "color": {
@@ -78,46 +78,6 @@ const particleSettings = {
         }
       }
     },
-    "interactivity": {
-      "detect_on": "window",
-      "events": {
-        "onhover": {
-          "enable": true,
-          "mode": "repulse"
-        },
-        "onclick": {
-          "enable": true,
-          "mode": "push"
-        },
-        "resize": true
-      },
-      "modes": {
-        "grab": {
-          "distance": 400,
-          "line_linked": {
-            "opacity": 1
-          }
-        },
-        "bubble": {
-          "distance": 400,
-          "size": 40,
-          "duration": 2,
-          "opacity": 8,
-          "speed": 3
-        },
-        "repulse": {
-          "distance": 200,
-          "duration": 0.4
-        },
-        "push": {
-          "particles_nb": 4
-        },
-        "remove": {
-          "particles_nb": 2
-        }
-      }
-    },
-    "retina_detect": true
   }
 
 const initialState = {
